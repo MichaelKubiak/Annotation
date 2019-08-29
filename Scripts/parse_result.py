1ccb53c327329a063aefa739777d529a6a48cfe6
Michael Kubiak <michael.kubiak@bath.edu>
2019-08-29 13:40:43 +0100

    parse file produces matrix and outputs it along with row and column names

--- Scripts/parse_result.py
+++ Scripts/parse_result.py
@@ -5,8 +5,9 @@
 
 from pathlib import Path
 import re
+from scipy import sparse
 import numpy as np
-import pandas
+from scipy import io
 # ------------------------------------------------------------------------------------------------------
 # Useful variables
 
@@ -14,9 +15,9 @@ HOME = str(Path.home())
 DATA = HOME + "/Annotation/Data/"
 
 # ------------------------------------------------------------------------------------------------------
-# Make empty matrix
+# Make empty sparse matrix
 
-with open(DATA+ "Pfam-A.hmm") as pfamfile:
+with open(DATA + "Pfam-A.hmm", encoding="utf-8") as pfamfile:
     pfam = pfamfile.readlines()
 
 pfam_Accessions = []
@@ -32,18 +33,29 @@ for line in fasta:
     if line.startswith(">"):
         protein_Accessions.append(line.split("|")[1])
 
-zeros = np.zeros(shape=(len(pfam_Accessions), len(protein_Accessions)))
-frame = pandas.DataFrame(zeros, columns=pfam_Accessions, index=protein_Accessions)
+mat = sparse.lil_matrix((len(protein_Accessions), len(pfam_Accessions)), dtype=np.float32)
 
-print(frame)
 
 # ------------------------------------------------------------------------------------------------------
-with open(DATA + "hmmresult_56000vs1800") as infile:
+# put the searchhmm results from the table into the matrix
+
+with open(DATA + "hmmresult_full") as infile:
     result = infile.readlines()
 
+
+scores = []
 for line in result:
     if not line.startswith("#"):
-        splitline = re.split(r"\s", line)
+        splitline = list(filter(None, re.split(r"\s", line)))
+        scores.append((splitline[0].split("|")[1], splitline[3], splitline[5]))
 
+for score in scores:
+    mat[protein_Accessions.index(score[0]), pfam_Accessions.index(score[1])] = score[2]
+
+print(mat.data.nbytes)
+io.mmwrite(DATA + "score_matrix")
+with open(DATA + "matrix_rows", "w") as rows, open(DATA + "matrix_columns", "w") as columns:
+    rows.write(str(protein_Accessions))
+    columns.write(str(pfam_Accessions))
 
 
