c109aea620cd4a09c2b7ba34e8bc64e702c09db4
Michael Kubiak <michael.kubiak@bath.edu>
2019-09-05 12:38:00 +0100

    Multiprocessed calculate_stats

--- Scripts/calculate_stats.py
+++ Scripts/calculate_stats.py
@@ -5,6 +5,8 @@
 
 from paths import DATA
 import re
+import concurrent.futures
+
 
 # ------------------------------------------------------------------------------------------------------
 # Calculate percentage non enzyme
@@ -16,14 +18,11 @@ targets = list(map(str.strip, targets))
 
 print("Non-enzyme: %.2f%%" % (targets.count("None")/len(targets)*100))
 
+
 # ------------------------------------------------------------------------------------------------------
-# Find HMMs not represented in Swissprot
+# Function to be multithreaded in next section - finds unused hidden markov models
 
-with open(DATA + "hmmresult_full") as resultsfile, open(DATA + "matrix_columns") as pfamfile:
-    results, pfam = resultsfile.readlines(), pfamfile.readlines()
-
-unused = []
-for line in pfam:
+def find_unused(line, results):
     present = False
     for row in results:
         if not row.startswith("#"):
@@ -33,17 +32,30 @@ for line in pfam:
 
                 break
     if not present:
-        unused.append(line)
-print("Unused HMMs: number = %d %s" % (len(unused), str(unused)))
+        return line
+
 
 # ------------------------------------------------------------------------------------------------------
-# Find Proteins not represented in Pfam
+# Find HMMs not represented in Swissprot
 
-with open(DATA + "matrix_rows") as spfile:
-    swissprot = spfile.readlines()
+with open(DATA + "hmmresult_56000vs1800") as resultsfile, open(DATA + "matrix_columns") as pfamfile:
+    results, pfam = resultsfile.readlines(), pfamfile.readlines()
 
-unmodelled = []
-for line in swissprot:
+unused = []
+executor = concurrent.futures.ProcessPoolExecutor(20)
+futures = [executor.submit(find_unused, line, results) for line in pfam]
+concurrent.futures.wait(futures)
+
+for future in futures:
+    unused.append(future.result())
+
+print("Unused HMMs: number = %d %s" % (len(unused), str(unused)))
+
+
+# ------------------------------------------------------------------------------------------------------
+# Function to be multithreaded in next section - finds unmodelled proteins
+
+def find_unmodelled(line, results):
     present = False
     for row in results:
         if not row.startswith("#"):
@@ -52,5 +64,18 @@ for line in swissprot:
                 present = True
                 break
     if not present:
-        unmodelled.append(line)
+        return line
+
+
+# ------------------------------------------------------------------------------------------------------
+# Find Proteins not represented by Pfam HMMs
+
+with open(DATA + "matrix_rows") as spfile:
+    swissprot = spfile.readlines()
+
+unmodelled = []
+futures = [executor.submit(find_unmodelled, line, results) for line in swissprot]
+concurrent.futures.wait(futures)
+for future in futures:
+    unmodelled.appen(future.result())
 print("Unrepresented proteins: number = %d %s" % (len(unmodelled), str(unmodelled)))
