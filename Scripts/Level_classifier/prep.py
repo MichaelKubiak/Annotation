d89a359138ee506067faf313903602fb1efd9f05
michaelkubiak <michael.kubiak@bath.edu>
2020-03-19 16:48:40 +0000

    Restart work on level classifier
    - fix a few bugs in the prep module
    - start to implement training module

--- Scripts/Level_classifier/prep.py
+++ Scripts/Level_classifier/prep.py
@@ -4,12 +4,39 @@
 
 import random
 
+
 # ------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------
 # Function to split a dataset and target list into training and test sets
 
-def train_test_split_sparse(data, targets, test_size, level=1, prev_class="None", random_state=0):
-    if prev_class is not "None":
-        current = [x for x in len(targets) if targets[x].split(".")[:level] == prev_class]
+def train_test_split(data, targets, test_size,  prev_class, level=0, random_state=0):
+    if level != 0:
+        current = [x for x in range(len(targets)) if prev_class in [".".join(target.split(".")[:level]) for target in targets[x].split("\t")]]
     else:
         current = range(len(targets))
+
+    X_shuffle, y_shuffle = rearrange(data[current], [targets[x] for x in current], level, random_state)
+
+    X_train = X_shuffle[round(len(current)*test_size):]
+    X_test = X_shuffle[:round(len(current)*test_size)]
+
+    y_train = y_shuffle[round(len(current)*test_size):]
+    y_test = y_shuffle[:round(len(current)*test_size)]
+    return X_train, X_test, y_train, y_test
+
+
+# ------------------------------------------------------------------------------------------------------
+# Function to rearrange the data and targets
+
+def rearrange(data, targets, level, random_state):
+
+    rearr = list(range(data.shape[0]))
+    random.seed(random_state)
+    random.shuffle(rearr)
+    X_shuffle = data[rearr]
+    # the levelth number from each \t separated group in the list
+    y = ["\t".join([num.split(".")[level] for num in nums]) for nums in [target.split("\t") for target in targets]]
+
+    y_shuffle = [y[i] for i in rearr]
+
+    return X_shuffle, y_shuffle
--- Scripts/Level_classifier/tf_neural_network_per_level.py
+++ Scripts/Level_classifier/tf_neural_network_per_level.py
@@ -2,14 +2,14 @@
 # ------------------------------------------------------------------------------------------------------
 # Imports
 
-from Regressors import prep
+from Level_classifier import prep
 import numpy as np
 import tensorflow as tf
-from scipy.sparse import vstack
-import math
 
 
 # ------------------------------------------------------------------------------------------------------
 # train level - train the current level of the network
 
-def train_level(prev_class, level, hidden, nodes, activation):
+def train_level(data, targets, level, n_hidden, n_nodes, activation, prev_class="None", random_state=0):
+
+    X_train, X_test, y_train, y_test = prep.train_test_split(data, targets, prev_class, level, random_state)
