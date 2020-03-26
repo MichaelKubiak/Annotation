5c2abb4045bcadd271b52fb17ab90c7311569d48
michaelkubiak <michael.kubiak@bath.edu>
2020-03-26 11:59:55 +0000

    Change the output of train_test_split method to provide all targets (for a given protein) as separate datapoints, while avoiding duplication of large data.
    Start on neural network

--- Scripts/Level_classifier/prep.py
+++ Scripts/Level_classifier/prep.py
@@ -9,34 +9,35 @@ import random
 # ------------------------------------------------------------------------------------------------------
 # Function to split a dataset and target list into training and test sets
 
-def train_test_split(data, targets, test_size,  prev_class, level=0, random_state=0):
+def train_test_split(targets, test_size,  prev_class, level=0, random_state=0):
     if level != 0:
         current = [x for x in range(len(targets)) if prev_class in [".".join(target.split(".")[:level]) for target in targets[x].split("\t")]]
     else:
         current = range(len(targets))
 
-    X_shuffle, y_shuffle = rearrange(data[current], [targets[x] for x in current], level, random_state)
+    pairs = rearrange(targets, current, level, random_state)
 
-    X_train = X_shuffle[round(len(current)*test_size):]
-    X_test = X_shuffle[:round(len(current)*test_size)]
+    pairs_train = pairs[round(len(current)*test_size):]
+    pairs_test = pairs[:round(len(current)*test_size)]
 
-    y_train = y_shuffle[round(len(current)*test_size):]
-    y_test = y_shuffle[:round(len(current)*test_size)]
-    return X_train, X_test, y_train, y_test
+    return pairs_train, pairs_test
 
 
 # ------------------------------------------------------------------------------------------------------
 # Function to rearrange the data and targets
 
-def rearrange(data, targets, level, random_state):
+def rearrange(targets, current, level, random_state):
 
-    rearr = list(range(data.shape[0]))
+    # create tuples of (matrix row number, classification)
+    pairs = []
+    for i in range(len(targets)) and current:
+        for target in targets[i].split("\t"):
+            pairs.append((i, target.split(".")[level]))
+
+    # randomly rearrange the tuples in the list
+    rearr = list(range(len(pairs)))
     random.seed(random_state)
     random.shuffle(rearr)
-    X_shuffle = data[rearr]
-    # the levelth number from each \t separated group in the list
-    y = ["\t".join([num.split(".")[level] for num in nums]) for nums in [target.split("\t") for target in targets]]
+    pairs_shuffle = [pairs[i] for i in rearr]
 
-    y_shuffle = [y[i] for i in rearr]
-
-    return X_shuffle, y_shuffle
+    return pairs_shuffle
--- Scripts/Level_classifier/tf_neural_network_per_level.py
+++ Scripts/Level_classifier/tf_neural_network_per_level.py
@@ -10,6 +10,30 @@ import tensorflow as tf
 # ------------------------------------------------------------------------------------------------------
 # train level - train the current level of the network
 
-def train_level(data, targets, level, n_hidden, n_nodes, activation, prev_class="None", random_state=0):
+def train_level(data, targets, level, n_hidden, n_nodes, activations, prev_class="None", random_state=0):
 
     X_train, X_test, y_train, y_test = prep.train_test_split(data, targets, prev_class, level, random_state)
+
+    # find number of input features
+    n_features = X_train.shape[1]
+
+    # set numpy and tensorflow seeds
+    np.random.seed(random_state)
+    tf.random.set_seed(random_state)
+
+     # if only one number of nodes or activation function is given, multiply it by the number of hidden layers so that all hidden layers have those values
+    if n_hidden != 1:
+        if type(n_nodes) is int:
+            n_nodes = [n_nodes] * n_hidden
+        if type(activations) is str:
+            activations = [activations] * n_hidden
+
+    # make a Sequential neural network
+    nn = tf.keras.Sequential()
+    # add the first hidden layer, which has inputs of the correct shape (the number of input features)
+    nn.add(tf.keras.layers.Dense(n_nodes[0], activation=activations[0], input_shape=(n_features,)))
+    # create the remaining hidden layers
+    for i in range(1, n_hidden):
+        nn.add(tf.keras.layers.Dense(n_nodes[i], activation=activations[i]))
+    # add the output layer with as many nodes as there are classifications
+    nn.add()
