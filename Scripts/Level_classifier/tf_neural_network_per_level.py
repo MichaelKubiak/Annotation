ad5de163816f3fe3e1a7acce7684b732857ba884
michaelkubiak <michael.kubiak@bath.edu>
2020-04-16 11:31:41 +0100

    Implement network building

--- Scripts/Level_classifier/tf_neural_network_per_level.py
+++ Scripts/Level_classifier/tf_neural_network_per_level.py
@@ -5,12 +5,14 @@
 from Level_classifier import prep
 import numpy as np
 import tensorflow as tf
+import math
+from scipy.sparse import vstack
 
 
 # ------------------------------------------------------------------------------------------------------
 # train level - train the current level of the network
 
-def train_level(data, targets, level, n_hidden, n_nodes, activations, prev_class="None", random_state=0):
+def train_level(data, targets, level, n_hidden, n_nodes, activations, n_epochs, batch_size, prev_class="None", random_state=0):
 
     train, test, unique_targets = prep.train_test_split(targets, prev_class, level, random_state)
 
@@ -36,4 +38,41 @@ def train_level(data, targets, level, n_hidden, n_nodes, activations, prev_class
     for i in range(1, n_hidden):
         nn.add(tf.keras.layers.Dense(n_nodes[i], activation=activations[i]))
     # add the output layer with as many nodes as there are classifications
-    nn.add(tf.keras.layers.Dense())
+    nn.add(tf.keras.layers.Dense(len(unique_targets), "sigmoid"))
+    # compile the neural network
+    nn.compile(optimizer="rmsprop", loss="mse", metrics=["accuracy"])
+    # make a generator for the batches to reduce RAM use and fit the network to the generated batches
+    nn.fit_generator(generate_batch(data, targets, train, batch_size, n_epochs, random_state), round(len(train)/batch_size), epochs=n_epochs)
+    # evaluate the network on batches of the test dataset
+    print(nn.evaluate_generator(generate_batch(data, targets, test, batch_size, n_epochs, random_state), round(len(test)/batch_size)))
+
+# ------------------------------------------------------------------------------------------------------
+# Function to generate a data-target batch of the correct size
+
+def generate_batch(data, targets, train, batch_size, epochs=1, random_state=0, keep_duplicate=True):
+    # generate a new set of data for each epoch
+    for i in range(epochs):
+        # rearrange the training dataset and targets
+        train_rearr=prep.rearrange(train, random_state+i)
+        # generate the correct number of batches for the dataset
+        for j in range(math.ceil(len(train)/batch_size)):
+            # create the batches
+            X_batch = [data[x] for x in train_rearr[j*batch_size:(j+1)*batch_size]]
+            y_batch = [targets[x] for x in train_rearr[j*batch_size:(j+1)*batch_size]]
+            # split each protein with multiple ECs into separate lines
+            for protein in range(len(y_batch)):
+                if len(y_batch[protein]) > 1:
+                    for EC in range(len(y_batch[protein])):
+                        if EC != 0 and keep_duplicate:
+                            # Duplicate that row of matrix at the bottom of the matrix
+                            X_batch = vstack((X_batch, X_batch[protein]))
+                            # add the EC to the end of y_batch
+                            y_batch.append([y_batch[protein][EC]])
+                    # set the original occurrence to the first EC that was present
+                    y_batch[protein] = [y_batch[protein][0]]
+            # flatten the list into a list of strings, rather than a list of lists
+            y_batch = [y for y_list in y_batch for y in y_list]
+            yield X_batch, y_batch
+
+
+
